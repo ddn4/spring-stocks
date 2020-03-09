857111a25685792eea14726c26c4ec4f18776c50
Dan Nemeth <dan@dan.local>
2020-03-09 14:27:42 -0400

    initial commit

---
+++ stock-service/src/test/java/com/vmware/labs/stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmware.labs.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
