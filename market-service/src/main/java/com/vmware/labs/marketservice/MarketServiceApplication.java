c8ad1e0290e09ccfdb8f425dbf183a0a7e741b05
Daniel Frey <dmfrey@gmail.com>
2021-12-13 12:00:44 -0500

    move spring-native to branch and revert

--- market-service/src/main/java/com/vmware/labs/marketservice/MarketServiceApplication.java
+++ market-service/src/main/java/com/vmware/labs/marketservice/MarketServiceApplication.java
@@ -2,10 +2,8 @@ package com.vmware.labs.marketservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.nativex.hint.NativeHint;
 
 @SpringBootApplication
-@NativeHint( options = "--trace-object-instantiation=ch.qos.logback.classic.Logger" )
 public class MarketServiceApplication {
 
 	public static void main(String[] args) {
